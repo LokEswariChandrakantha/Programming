623906e084a432b8bc8253c8a31dbe461bc745d4
lokeswari <lokeswarichandrakantha@gmail.com>
2026-09-19 22:03:02 +0530

    pushing program

---
+++ src/Programs/SwapingOfTwoNumbers.java
@@ -0,0 +1,12 @@
+package Programs;
+
+public class SwapingOfTwoNumbers {
+public static void main(String[] args) {
+	int a=20;
+	int b=30;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	System.out.println(a+" "+b);
+}
+}
